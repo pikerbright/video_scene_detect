807f0b82b676c002d6c2ebf952d7f4acb4ccf03b
zhuliang <zhuliang@qiniu.com>
2018-05-10 17:21:34 +0800

    add json result output

--- scene_detect.py
+++ scene_detect.py
@@ -32,6 +32,7 @@ def write_scene_file(filename):
         if frame_info['media_type'] == 'video' and frame_info['pict_type'] == 'I':
             pts_times.append(float(frame_info['pkt_pts_time']))
 
+    detect_result = []
     with open(filename, 'w') as f:
         for i in range(len(pts_times) - 1):
             start_pts_time = pts_times[i]
@@ -47,11 +48,22 @@ def write_scene_file(filename):
 
             f.write("{:0>2}:{:0>2}:{:.3f} --> {:0>2}:{:0>2}:{:.3f}\n".format(s_hour, s_minute, s_second, e_hour,
                                                                              e_minute, e_second))
-            f.write("****镜头{}****\n\n".format(i + 1))
+            f.write("镜头{}\n\n".format(i + 1))
+            str_start_time = "{:0>2}:{:0>2}:{:.3f}".format(s_hour, s_minute, s_second)
+            str_end_time = "{:0>2}:{:0>2}:{:.3f}".format(e_hour, e_minute, e_second)
+            detect_result.append({'index': i + 1, 'start_time': str_start_time, 'end_time': str_end_time})
 
         f.write("{}\n".format(i + 1))
-        f.write("{:0>2}:{:0>2}:{:.3f} --> {:0>2}:{:0>2}:{:.3f}\n".format(e_hour, e_minute, e_second, 1, 0, 0))
-        f.write("****镜头{}****\n\n".format(i + 1))
+        f.write("{:0>2}:{:0>2}:{:.3f} --> {:0>2}:{:0>2}:{:.3f}\n".format(e_hour, e_minute, e_second, 10, 0, 0))
+        f.write("镜头{}\n\n".format(i + 1))
+
+        str_start_time = "{:0>2}:{:0>2}:{:.3f}".format(e_hour, e_minute, e_second)
+        str_end_time = "{:0>2}:{:0>2}:{:.3f}".format(10, 0, 0)
+        detect_result.append({'index': i + 1, 'start_time': str_start_time, 'end_time': str_end_time})
+
+    json_file_name = filename.split('.')[0] + '.json'
+    with open(json_file_name, 'w') as f:
+        json.dump(detect_result, f)
 
 
 if __name__ == "__main__":
